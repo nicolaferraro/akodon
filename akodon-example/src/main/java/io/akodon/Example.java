7e5595b1897f7bfd8f4bb1d665cc8499da979d3d
Nicola Ferraro <ni.ferraro@gmail.com>
2016-07-13 14:22:55 +0200

    working hello world on quickstart.cloudera

---
+++ akodon-example/src/main/java/io/akodon/Example.java
@@ -0,0 +1,15 @@
+package io.akodon;
+
+/**
+ *
+ */
+public class Example {
+
+    public static void main(String[] args) throws Exception {
+        for (int i = 0; i < 500; i++) {
+            System.out.println("Hello Yarn!");
+            Thread.sleep(1000);
+        }
+    }
+
+}
